3771817cce54d3399f5627adcd4affccd8517955
east32 <46858464+east32@users.noreply.github.com>
2019-08-26 15:27:55 +0300

    подогнал под пиксель, почти полностью, отсался только JS для валидации и для IE 11 подправить

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,14 @@
 var navMain = document.querySelector('.main-nav');
-            var navToggle = document.querySelector('.main-nav__toggle');
+
+var navToggle = document.querySelector('.main-nav__toggle');
+
+var form = document.querySelector("form");
+var name = document.querySelector("[name=name]");
+var weight = document.querySelector("[name=weight]");
+var mail = document.querySelector("[name=email]");
+var phone = document.querySelector("[name=phone]");
+
+
 
             navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +19,11 @@ var navMain = document.querySelector('.main-nav');
               } else {
                 navMain.classList.add('main-nav--closed');
                 navMain.classList.remove('main-nav--opened');
+              }
+            });
+
+            form.addEventListener("submit", function (evt) {
+              if (!name.value || !weight.value ||  !mail.value || !phone.value) {
+                evt.preventDefault();
               }
             });
