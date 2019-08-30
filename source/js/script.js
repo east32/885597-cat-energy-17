06d7e5ee148cd31ac2f8c668df591b653e39a7f5
east32 <46858464+east32@users.noreply.github.com>
2019-08-30 21:24:27 +0300

    для защиты

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,11 @@ var navMain = document.querySelector('.main-nav');
 
 var navToggle = document.querySelector('.main-nav__toggle');
 
-var form = document.querySelector("form");
-var name = document.querySelector("[name=name]");
-var weight = document.querySelector("[name=weight]");
-var mail = document.querySelector("[name=email]");
-var phone = document.querySelector("[name=phone]");
+var form = document.querySelector('form');
+var petname = document.querySelector('[name=petname]');
+var weight = document.querySelector('[name=weight]');
+var mail = document.querySelector('[name=email]');
+var phone = document.querySelector('[name=phone]');
 
 
 
@@ -21,9 +21,12 @@ var phone = document.querySelector("[name=phone]");
                 navMain.classList.remove('main-nav--opened');
               }
             });
-
-            form.addEventListener("submit", function (evt) {
-              if (!name.value || !weight.value ||  !mail.value || !phone.value) {
+            form.addEventListener('submit', function (evt) {
+               if (this.petname.is|| !weight.value || !email.value || !phone.value) {
                 evt.preventDefault();
-              }
+                petname.classList.add (error-input);
+                weight.classList.add (error-input);
+                email.classList.add (error-input);
+                phone.classList.add (error-input);
+               }
             });
